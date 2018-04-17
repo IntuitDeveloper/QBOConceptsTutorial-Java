13b74d13ea697b59515f47ad446b21b236e9a017
Tandra Sishtla, Sunil <Sunil_TandraSishtla@intuit.com>
2018-04-17 12:14:20 -0700

    Implemented "landing the job" task

--- src/main/java/com/intuit/developer/tutorials/controller/JobsController.java
+++ src/main/java/com/intuit/developer/tutorials/controller/JobsController.java
@@ -1,10 +1,35 @@
 package com.intuit.developer.tutorials.controller;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.intuit.ipp.core.Context;
+import com.intuit.ipp.core.ServiceType;
+import com.intuit.ipp.data.Account;
+import com.intuit.ipp.data.AccountTypeEnum;
+import com.intuit.ipp.data.Customer;
+import com.intuit.ipp.data.DiscountLineDetail;
+import com.intuit.ipp.data.Estimate;
+import com.intuit.ipp.data.Invoice;
+import com.intuit.ipp.data.Item;
+import com.intuit.ipp.data.ItemTypeEnum;
+import com.intuit.ipp.data.Line;
+import com.intuit.ipp.data.LineDetailTypeEnum;
+import com.intuit.ipp.data.ReferenceType;
+import com.intuit.ipp.data.SalesItemLineDetail;
+import com.intuit.ipp.security.OAuth2Authorizer;
+import com.intuit.ipp.util.Config;
+import com.intuit.ipp.util.DateUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +50,9 @@ import com.intuit.ipp.services.DataService;
  */
 @Controller
 public class JobsController {
-	
+
+	public static final String MINOR_VERSION = "4";
+
 	@Autowired
 	OAuth2PlatformClientFactory factory;
 	
@@ -55,26 +82,60 @@ public class JobsController {
         try {
         	
         	//get DataService
-    		DataService service = helper.getDataService(realmId, accessToken);
-			
-    		//add customer
-    		
-    		//add item
+    		DataService service = getDataService(realmId, accessToken, MINOR_VERSION);
+
+			//add customer
+			Customer customer = getCustomerWithMandatoryFields();
+
+			final Customer customerResult = service.add(customer);
+
+			//add item
+
+			Item item = getItemWithMandatoryFields(service);
+
+			final Item itemResult = service.add(item);
     		
     		//create estimate
-    		
-    		//update estimate -change amt
-			
+			Estimate estimate = getEstimateWithMandatoryFields(itemResult, customerResult, service);
+
+			final Estimate createdEstimate = service.add(estimate);
+
+			//update estimate -change amt
+
+			createdEstimate.setTotalAmt(new BigDecimal("400.00"));
+
+			final Estimate updatedEstimate = service.update(createdEstimate);
+
 			//create invoice using estimate data
-    		
-    		//update invoice to add discount
-    		
-    		//return response back  		
-    		return "";
+
+			Invoice invoice = getInvoiceFieldsFromEstimate(estimate);
+
+			final Invoice createdInvoice = service.add(invoice);
+
+			//update invoice to add discount
+
+			DiscountLineDetail discountLineDetail = new DiscountLineDetail();
+
+			discountLineDetail.setPercentBased(false);
+			discountLineDetail.setDiscountAccountRef(createAccountRef(findAccountByType(AccountTypeEnum.INCOME, service)));
+
+			Line discountLine = new Line();
+
+			discountLine.setDiscountLineDetail(discountLineDetail);
+			discountLine.setDetailType(LineDetailTypeEnum.DISCOUNT_LINE_DETAIL);
+			discountLine.setAmount(new BigDecimal("10.00"));
+
+			createdInvoice.getLine().add(discountLine);
+
+			createdInvoice.setSparse(true);
+
+			final Invoice updatedInvoice = service.update(createdInvoice);
+
+			//return response back
+    		return createResponse(updatedInvoice);
 			
 		}
-	        
-        catch (InvalidTokenException e) {			
+	    catch (InvalidTokenException e) {
 			return new JSONObject().put("response","InvalidToken - Refreshtoken and try again").toString();
 		} catch (FMSException e) {
 			List<Error> list = e.getErrorList();
@@ -82,5 +143,176 @@ public class JobsController {
 			return new JSONObject().put("response","Failed").toString();
 		}
     }
-    
+
+	/**
+	 * This method is an override with duplication of the QBOServiceHelper
+	 * to pass in a different minor version.  This is due to an invoice update
+	 * failing with the discount line for minor version "23" (SDK default), hence
+	 * overriding with the minor version "4"
+	 *
+	 * @param realmId
+	 * @param accessToken
+	 * @return
+	 * @throws FMSException
+	 */
+	private DataService getDataService(String realmId, String accessToken, String minorVersion) throws FMSException {
+
+		//get DataService
+		String url = factory.getPropertyValue("IntuitAccountingAPIHost") + "/v3/company";
+
+		Config.setProperty(Config.BASE_URL_QBO, url);
+		//create oauth object
+		OAuth2Authorizer oauth = new OAuth2Authorizer(accessToken);
+
+		Context context = new Context(oauth, ServiceType.QBO, realmId);
+		context.setMinorVersion(minorVersion);
+
+		// create dataservice
+		return new DataService(context);
+	}
+
+
+	private Invoice getInvoiceFieldsFromEstimate(Estimate estimate) {
+		Invoice invoice = new Invoice();
+
+		invoice.setCustomerRef(estimate.getCustomerRef());
+
+		List<Line> invoiceLine = new ArrayList<Line>();
+
+
+		Line line = null;
+
+		if(estimate.getLine()!=null && !estimate.getLine().isEmpty()) {
+			line = estimate.getLine().get(0);
+		}
+
+		SalesItemLineDetail silDetails = new SalesItemLineDetail();
+
+		if(estimate.getLine()!=null && !estimate.getLine().isEmpty()
+				&& estimate.getLine().get(0).getSalesItemLineDetail()!=null ) {
+			silDetails.setItemRef(estimate.getLine().get(0).getSalesItemLineDetail().getItemRef());
+		}
+
+		line.setSalesItemLineDetail(silDetails);
+		invoiceLine.add(line);
+		invoice.setLine(invoiceLine);
+
+		return invoice;
+	}
+
+	private Estimate getEstimateWithMandatoryFields(Item item, Customer customer, DataService service) throws FMSException {
+		Estimate estimate = new Estimate();
+		estimate.setDocNumber(RandomStringUtils.randomNumeric(4));
+		try {
+			estimate.setTxnDate(DateUtils.getCurrentDateTime());
+		} catch (ParseException e) {
+			throw new FMSException("ParseException while getting current date.");
+		}
+		try {
+			estimate.setExpirationDate(DateUtils.getDateWithNextDays(15));
+		} catch (ParseException e) {
+			throw new FMSException("ParseException while getting current date + 15 days.");
+		}
+
+		Line line1 = new Line();
+		line1.setLineNum(new BigInteger("1"));
+		line1.setAmount(new BigDecimal("300.00"));
+		line1.setDetailType(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+
+		SalesItemLineDetail salesItemLineDetail1 = new SalesItemLineDetail();
+		salesItemLineDetail1.setItemRef(createItemRef(item));
+
+		ReferenceType taxCodeRef1 = new ReferenceType();
+		taxCodeRef1.setValue("NON");
+		salesItemLineDetail1.setTaxCodeRef(taxCodeRef1);
+		line1.setSalesItemLineDetail(salesItemLineDetail1);
+
+		List<Line> lines1 = new ArrayList<>();
+		lines1.add(line1);
+		estimate.setLine(lines1);
+
+
+		Account depositAccount = findAccountByType(AccountTypeEnum.BANK, service);
+		estimate.setDepositToAccountRef(createAccountRef(depositAccount));
+
+		estimate.setCustomerRef(createCustomerRef(customer));
+
+		estimate.setApplyTaxAfterDiscount(false);
+		estimate.setTotalAmt(new BigDecimal("300.00"));
+		estimate.setPrivateNote("Accurate Estimate");
+
+		return estimate;
+	}
+
+	private Account findAccountByType(AccountTypeEnum accountTypeEnum, DataService service) throws FMSException {
+		final List<Account> accountList = service.findAll(new Account());
+
+		final Iterator<Account> accountIterator = accountList.iterator();
+
+		while (accountIterator.hasNext()) {
+			final Account account = accountIterator.next();
+			if(account.getAccountType() == accountTypeEnum) {
+				return account;
+			}
+		}
+		return null;
+	}
+
+	private Customer getCustomerWithMandatoryFields() {
+		Customer customer = new Customer();
+		customer.setDisplayName(RandomStringUtils.randomAlphanumeric(6));
+		return customer;
+	}
+
+	private Item getItemWithMandatoryFields(DataService service) throws FMSException {
+		Item item = new Item();
+		item.setName("Item" + RandomStringUtils.randomAlphanumeric(5));
+		item.setActive(true);
+		item.setTaxable(false);
+		item.setUnitPrice(new BigDecimal("200"));
+		item.setType(ItemTypeEnum.SERVICE);
+
+		item.setIncomeAccountRef(createAccountRef(findAccountByType(AccountTypeEnum.INCOME, service)));
+		item.setExpenseAccountRef(createAccountRef(findAccountByType(AccountTypeEnum.EXPENSE, service)));
+		return item;
+	}
+
+	private ReferenceType createAccountRef(Account account) {
+		ReferenceType referenceType = new ReferenceType();
+		referenceType.setName(account.getName());
+		referenceType.setValue(account.getId());
+		return referenceType;
+	}
+
+	private ReferenceType createItemRef(Item item) {
+		ReferenceType referenceType = new ReferenceType();
+		referenceType.setName(item.getName());
+		referenceType.setValue(item.getId());
+		return referenceType;
+	}
+
+	private ReferenceType createCustomerRef(Customer customer) {
+		ReferenceType referenceType = new ReferenceType();
+		referenceType.setName(customer.getFullyQualifiedName());
+		referenceType.setValue(customer.getId());
+		return referenceType;
+	}
+
+	private String createResponse(Object entity) {
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonInString;
+		try {
+			jsonInString = mapper.writeValueAsString(entity);
+		} catch (JsonProcessingException e) {
+			return createErrorResponse(e);
+		} catch (Exception e) {
+			return createErrorResponse(e);
+		}
+		return jsonInString;
+	}
+
+	private String createErrorResponse(Exception e) {
+		logger.error("Exception while getting company info ", e);
+		return new JSONObject().put("response","Failed").toString();
+	}
 }
