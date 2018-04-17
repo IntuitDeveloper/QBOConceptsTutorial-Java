f8f400503b6a758d33c147861fcdeafa18ceaff8
brandonc418 <brandon_cole@intuit.com>
2018-04-17 14:45:01 -0700

    Remove unused import.

--- src/main/java/com/intuit/developer/tutorials/controller/InventoryController.java
+++ src/main/java/com/intuit/developer/tutorials/controller/InventoryController.java
@@ -3,7 +3,6 @@ package com.intuit.developer.tutorials.controller;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
